2b37850778dc06a0932863cd55c8f27d0b69ef26
Benjamin Jones <BenjaminPaikJones@gmail.com>
2018-06-13 19:39:24 -0400

    Add DROP TABLES before creation on init.

--- gaime-competition/db/schema.sql
+++ gaime-competition/db/schema.sql
@@ -1,33 +1,37 @@
+DROP TABLE IF EXISTS Users;
 CREATE TABLE Users (
 	user_id INT AUTO_INCREMENT NOT NULL,
 	email VARCHAR(30) NOT NULL,
 	password VARCHAR(30) NOT NULL,
-	priveleges TINYINT NOT NULL DEFAULT 0,
+	privileges TINYINT NOT NULL DEFAULT 0,
 	fname VARCHAR(30),
 	lname VARCHAR(30),
 	PRIMARY KEY (user_id)
 	);
-	
+
+DROP TABLE IF EXISTS Languages;	
 CREATE TABLE Languages (
 	language_id INT AUTO_INCREMENT NOT NULL,
-	name varchar(30) NOT NULL,
+	name VARCHAR(30) NOT NULL,
 	PRIMARY KEY (language_id)
 	);
 	
+DROP TABLE IF EXISTS Games;
 CREATE TABLE Games (
 	game_id INT AUTO_INCREMENT NOT NULL,
 	name VARCHAR(30) NOT NULL,
 	max_num_players INT NOT NULL,
-	default_player int,
+	default_player INT,
 	PRIMARY KEY (game_id)
 	);
 
+DROP TABLE IF EXISTS Players;
 CREATE TABLE Players (
 	player_id INT AUTO_INCREMENT NOT NULL,
 	file_location VARCHAR(30),
-	language_id int NOT NULL,
-	game_id int NOT NULL,
-	user_id int NOT NULL,
+	language_id INT NOT NULL,
+	game_id INT NOT NULL,
+	user_id INT NOT NULL,
 	PRIMARY KEY (player_id),
 	FOREIGN KEY (language_id) REFERENCES Languages (language_id),
 	FOREIGN KEY (game_id) REFERENCES Games (game_id),
@@ -36,6 +40,7 @@ CREATE TABLE Players (
 	
 ALTER TABLE Games ADD CONSTRAINT FOREIGN KEY (default_player) REFERENCES Players (player_id);
 
+DROP TABLE IF EXISTS Matches;
 CREATE TABLE Matches (
 	match_id INT AUTO_INCREMENT NOT NULL,
 	start_time DATETIME NOT NULL,
@@ -45,10 +50,11 @@ CREATE TABLE Matches (
 	FOREIGN KEY (game_id) REFERENCES Games (game_id)
 	);
 
+DROP TABLE IF EXISTS Friendships;
 CREATE TABLE Friendships (
 	requester_id INT NOT NULL,
 	responder_id INT NOT NULL,
-	status enum('S','R','D','A','U') NOT NULL,
+	status ENUM('S','R','D','A','U') NOT NULL,
 	request_time DATETIME NOT NULL,
 	response_time DATETIME,
 	PRIMARY KEY (requester_id, responder_id),
