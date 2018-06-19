becdd652fa6b4eee640b7c02dcba1f85d21c31bc
Ealautzenhiser <Ealautzenhiser@gmail.com>
2018-06-19 19:40:56 -0400

    Add matches to the DB

--- gaime/database/schema.sql
+++ gaime/database/schema.sql
@@ -58,8 +58,24 @@ CREATE TABLE Matches (
     start_time DATETIME NOT NULL,
     end_time DATETIME,
     game_id INT NOT NULL,
+    points INT NOT NULL DEFAULT 10,
+    referee_id INT NOT NULL,
+    winner_id INT,
     PRIMARY KEY (match_id),
-    FOREIGN KEY (game_id) REFERENCES Games (game_id)
+    FOREIGN KEY (game_id) REFERENCES Games (game_id),
+    FOREIGN KEY (referee_id) REFERENCES Uploads (upload_id),
+    FOREIGN KEY (winner_id) REFERENCES Uploads (upload_id)
+);
+
+CREATE TABLE Moves (
+	move_id INT AUTO_INCREMENT NOT NULL,
+	match_id INT NOT NULL,
+	turn_number INT NOT NULL,
+	player_id INT NOT NULL,
+	move VARCHAR(255),
+	PRIMARY KEY (move_id),
+	FOREIGN KEY (match_id) REFERENCES Matches (match_id),
+	FOREIGN KEY (player_id) REFERENCES Uploads (upload_id)
 );
 
 CREATE TABLE Friendships (
@@ -89,3 +105,4 @@ INSERT INTO Uploads (filename, language_id, game_id, author_id,
             '2018-06-18 23:59:59', 'Inactive', 'Player'),
            ('20180619120000_rock_paper_scissors.py', 1, 1, 1,
             '2018-06-19 12:00:00', 'Active', 'Player');
+            
