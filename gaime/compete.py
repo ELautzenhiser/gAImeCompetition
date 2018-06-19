fd7ef9c69c58aa26c98f31281a8e4cad1cba6cfe
Ealautzenhiser <Ealautzenhiser@gmail.com>
2018-06-19 19:12:30 -0400

    Show number of competitors per game

--- gaime/compete.py
+++ gaime/compete.py
@@ -9,8 +9,11 @@ bp = Blueprint('compete', __name__)
 
 @bp.route('/')
 def index():
-    game_query = 'SELECT g.name, g.max_num_players, u.username, g.created_dt' \
-                 ' FROM Games g JOIN Users u ON g.author_id = u.user_id' \
-                 ' ORDER BY game_id DESC'
+    game_query = 'SELECT g.name, g.max_num_players, u.username, g.created_dt, ' \
+                 'count(up.upload_id) as num_competitors ' \
+                 'FROM Games g INNER JOIN Users u ON g.author_id = u.user_id ' \
+                 'LEFT JOIN Uploads up on g.game_id=up.game_id ' \
+                 'WHERE up.active=\'Active\' AND up.type=\'Player\' ' \
+                 'GROUP BY g.game_id ORDER BY g.game_id DESC '
     games = query_db(game_query, 20)
     return render_template('compete/index.html', games=games)
