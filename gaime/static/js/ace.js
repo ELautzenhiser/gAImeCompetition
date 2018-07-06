a703c4c85e173cce89edd4291197874d34de71f6
Benjamin Jones <BenjaminPaikJones@gmail.com>
2018-07-05 22:03:48 -0400

    JavaScript magic.

--- gaime/static/js/ace.js
+++ gaime/static/js/ace.js
@@ -11,6 +11,9 @@ $(function () {
             height: textarea.height(),
             class: textarea.attr('class'),
         }).insertBefore(textarea);
+        if (textarea.data('editor-id')) {
+            editDiv[0].setAttribute('id', textarea.data('editor-id'));
+        }
         textarea.css('display', 'none');
         var editor = ace.edit(editDiv[0]);
         editor.renderer.setShowGutter(false);
@@ -22,5 +25,43 @@ $(function () {
         textarea.closest('form').submit(function () {
             textarea.val(editor.getSession().getValue());
         })
+    });
+});
+
+// Connects a file input to an ace editor
+$(function() {
+    $('input[type="file"][data-editor-class]').each(function() {
+        this.addEventListener('change', function (evt) {
+            var editorClass = evt.target.getAttribute('data-editor-class');
+            var els = document.getElementsByClassName(editorClass);
+
+            var Editor = ace.edit().constructor;
+            
+            var setText = function (text) {
+                for (var i = 0; i < els.length; i++) {
+                    var el = els.item(i);
+
+                    if (el.env && el.env.editor instanceof Editor) {
+                        editor = ace.edit(el);
+                        editor.setValue(text);
+                        continue;
+                    }
+                    if (el.tagName && el.tagName.toLowerCase() == "textarea") {
+                        el.value = text;
+                    }
+                }
+            };
+
+            var reader = new FileReader();
+            var text = null;
+
+            reader.onload = function(callback) {
+                return function(e) {
+                    t = e.target.result;
+                    callback(t);
+                };
+            } (setText);
+            reader.readAsText(evt.target.files[0]);
+        });
     });
 });
